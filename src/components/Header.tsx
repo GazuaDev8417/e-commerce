8cc473d4ffc19260a238671170117a6083dde784
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2023-08-09 00:32:30 -0300

    implementando menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, useState, useRef } from "react"
 import { BsPersonCircle } from 'react-icons/bs'
+import { IoIosClose } from 'react-icons/io'
 import styled from 'styled-components'
 
 
@@ -14,6 +15,45 @@ const Container = styled.div`
         cursor: pointer;
     }
 
+    .menu{
+        position: absolute;
+        top: 2vh;
+        right: -10vw;
+    }
+
+    ul{
+        list-style: none;
+        background-image: linear-gradient(lightgray, gray);
+        width: 10vw;
+        padding: 10px;
+        text-align: center;
+        border-radius: 5px;
+    }
+
+    ul .icon{
+        margin-bottom: 15px;
+    }
+
+    ul .close{
+        position: relative;
+        top: -4vh;
+        left: 2.5vw;
+        font-size: 15pt;
+        cursor: pointer;
+    }
+
+    li{
+        cursor: pointer;
+        margin: 5px;
+        text-align: left;
+    }
+
+    li:hover{
+        background-color: lightgray;
+        border-radius: 5px;
+        padding-left: 5px;
+    }
+
     img{
         width: 50px;
         cursor: pointer;
@@ -25,8 +65,27 @@ const Container = styled.div`
 
 const Header:FunctionComponent = ()=>{
     const inputFile = useRef<HTMLInputElement>(null)
+    const menu = useRef<HTMLDivElement>(null)
+    const [mode, setMode] = useState<Boolean>(false)
     const [profileImage, setProfileImage] = useState<undefined | string>()
 
+
+
+    const showMenu = ()=>{
+        if(!mode){
+            if(menu.current){
+                menu.current.style.right = '0.3vw'
+                menu.current.style.transition = '1s'  
+                setMode(true)          
+            }
+        }else{
+            if(menu.current){
+                menu.current.style.right = '-10vw'
+                menu.current.style.transition = '1s'
+                setMode(false) 
+            }
+        }
+    }
 
 
     const handleProfileImage = (e:React.ChangeEvent<HTMLInputElement>):void=>{
@@ -46,11 +105,23 @@ const Header:FunctionComponent = ()=>{
                 type="file" 
                 accept="image/*" 
                 onChange={handleProfileImage}/>
-            {profileImage ? (
-                <img src={profileImage} alt="Profile Image"
-                    onClick={()=> inputFile.current?.click()}/>
-            ) : <BsPersonCircle className='icon'
-                    onClick={()=> inputFile.current?.click()}/>}
+            <div>
+                {profileImage ? (
+                    <img src={profileImage} alt="Profile Image"/>
+                ) : <BsPersonCircle className='icon'
+                        onClick={showMenu}/>}
+                <div className="menu" ref={menu}>
+                    <ul>
+                        {/* <div className="topMenu"> */}
+                            <BsPersonCircle className='icon'/>
+                            <IoIosClose className='close' onClick={showMenu}/>
+                        {/* </div> */}
+                        <li>Usuário</li>
+                        <li>Conta</li>
+                        <li>Login</li>
+                    </ul>
+                </div>
+            </div>
         </Container>
     )
 }
