bf7086ffaf78554330a2dcc3a854106d2cc8957a
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2023-08-10 16:50:51 -0300

    Updates

--- assets/index-f918d3ad.js
+++ assets/index-f918d3ad.js
@@ -96,7 +96,7 @@ Error generating stack: `+o.message+`
         right: -10vw;
 
         @media(max-width: 600px){
-            top: -30vw;
+            top: -40vw;
         }
 
         @media(max-width: 300px){
