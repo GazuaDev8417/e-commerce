ddbd72877c57976e6e8cb63ea7767d5b8e50abd1
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2023-09-22 12:05:11 -0300

    adicionando links as tags a do footer

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 //HUMBURGER MENU
 
 const humburger = document.querySelector('.humburger')
+const inputEmail = document.querySelector('#inputEmail')
+const btnShare = document.querySelector('#btn-share')
 
 humburger.onclick = ()=>{
     const navBar = document.querySelector('.nav-bar')
     navBar.classList.toggle('active')
 }
+
+btnShare.addEventListener('click', async()=>{
+    try{
+        const shareData = {
+            text: inputEmail.value
+        }
+
+        if(inputEmail.value === ''){
+            alert('Preencha com seu endereço de email')
+            return
+        }
+
+        if(!navigator.share || !navigator.canShare){
+            alert('Navigador não suporta compartilhamento por desktop')
+            return
+        }
+        
+        await navigator.share(shareData)
+    }catch(error){
+        alert(error)
+    }
+})
