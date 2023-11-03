de521feb3ccbf7dfec361ae51c577f96c33ec677
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2023-11-03 00:29:19 -0300

    finalizando página do carrinho com opçãoes de pagamento

--- pages/products/products.js
+++ pages/products/products.js
@@ -1,32 +1,61 @@
 const gallery = Array.from(document.querySelectorAll('.image-gallery'))
 const darkbox = document.getElementById("darkbox")
 const darkboxImage = document.getElementById("darkbox-image")
+const productPrice = document.getElementById('product-price')
 const closeButton = document.getElementById("close-button")
 const prevButton = document.getElementById("prev-button")
 const nextButton = document.getElementById("next-button")
+const cartIcon = document.querySelector('#cart-icon')
 let currentImageIndex = 0;
+let cart = []
+
 
 
 
 const displayImage = (index)=>{
   const imageUrl = gallery[index].getAttribute('data-image')
+  const price = gallery[index].getAttribute('image-price')
+  
   darkboxImage.setAttribute('src', imageUrl)
+  productPrice.innerHTML = price
   currentImageIndex = index
 }
-
 
 gallery.forEach((image, index)=>{
   image.addEventListener('click', ()=>{
     displayImage(index)
     darkbox.style.display = 'block'
     document.body.style.overflow = 'hidden'
-  })
+  }) 
 })
 
+cartIcon.onclick = ()=>{
+  const imageUrl = gallery[currentImageIndex].getAttribute('data-image')
+  const price = gallery[currentImageIndex].getAttribute('image-price')
+  const cartObj = {
+    product: imageUrl,
+    price
+  }
+
+  const existingItem = cart.findIndex(item => item.product === imageUrl)
+
+  if(existingItem === -1){
+    cart.push(cartObj)
+    localStorage.setItem('cart', JSON.stringify(cart))  
+  }
+}
 
 closeButton.onclick = ()=>{
   darkbox.style.display = 'none'
+  document.body.style.overflow = 'auto'
 }
+
+document.addEventListener('keydown', (e)=>{
+  if(e.key === 'Escape' && darkbox.style.display === 'block'){
+    darkbox.style.display = 'none'
+    document.body.style.overflow = 'auto'
+  }
+})
 
 prevButton.addEventListener('click', ()=>{
   if(currentImageIndex > 0){
